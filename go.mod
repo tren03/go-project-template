0e41a35802c47dd1cbe680e39e6575e1511da5bc
vishnu <vishnusethuraman@gmail.com>
2024-09-21 17:14:13 +0530

    template commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tren03/go-project-template
+
+go 1.23.0
---
+++ internal/user_package/user_package.go
@@ -0,0 +1,8 @@
+package userpackage
+
+import "fmt"
+
+
+func UserPack(){
+    fmt.Println("This can be imported in main")
+}
